600d98f134ebef3fd8f6d15eb2e861565ebb0e1c
flyingdown <flyingdown@foxmail.com>
2019-08-07 14:42:37 +0800

    init

---
+++ convert.go
@@ -0,0 +1,55 @@
+package codeconvert
+
+import (
+	"bytes"
+	"io/ioutil"
+
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+)
+
+// Charset Charset
+type Charset string
+
+// utf-8
+const (
+	UTF8    = Charset("UTF-8") // utf-8
+	GB18030 = Charset("GB18030")
+	GBK     = Charset("GBK")
+)
+
+// ConvertByte2String ConvertByte2String
+func ConvertByte2String(byte []byte, charset Charset) string {
+	var str string
+	switch charset {
+	case GB18030:
+		var decodeBytes, _ = simplifiedchinese.GB18030.NewDecoder().Bytes(byte)
+		str = string(decodeBytes)
+	case GBK:
+		var decodeBytes, _ = simplifiedchinese.GBK.NewDecoder().Bytes(byte)
+		str = string(decodeBytes)
+	case UTF8:
+		fallthrough
+	default:
+		str = string(byte)
+	}
+	return str
+}
+
+func GbkToUtf8(s []byte) ([]byte, error) {
+	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GBK.NewDecoder())
+	d, e := ioutil.ReadAll(reader)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
+}
+
+func Utf8ToGbk(s []byte) ([]byte, error) {
+	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GBK.NewEncoder())
+	d, e := ioutil.ReadAll(reader)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/flyingdown/codeconvert
+
+go 1.12
